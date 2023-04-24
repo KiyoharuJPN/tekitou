7891718cb0559d44004ee71ed2656edacad4ef38
E21D2042 <st002122@m03.kyoto-kcg.ac.jp>
2023-04-24 17:15:04 +0900

    敵スクリプト修正

--- Assets/Script/Enemy/EnemyBase/Enemy.cs
+++ Assets/Script/Enemy/EnemyBase/Enemy.cs
@@ -121,7 +121,7 @@ public class Enemy : MonoBehaviour
         float y = Mathf.Sin(rad);
 
         //プレイヤーと自身の位置関係を調査
-        if(player.transform.position.y < this.transform.position.y) 
+        if(player.transform.position.y + 1f < this.transform.position.y) 
         { y = -y; }
         if(player.transform.position.x > this.transform.position.x) 
         { x = -x; }
