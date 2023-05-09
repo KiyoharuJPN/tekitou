28a6f0e46d5d979349354dc2894c32e50e74ff7d
LAPTOP-KIYOHARU\kiyoharu <1905088203@qq.com>
2023-05-09 21:38:16 +0900

    モンスターのスライムを作成

--- Assets/Script/Enemy/Slime.cs
+++ Assets/Script/Enemy/Slime.cs
@@ -1,5 +1,6 @@
 using System.Dynamic;
 using Unity.VisualScripting;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -9,16 +10,27 @@ using UnityEngine.UIElements;
 
 public class Slime : Enemy
 {
+    [Header("à⁄ìÆÇ∑ÇÈéûÇÃçÇÇ≥Ç∆ãóó£")]
+    public float moveHeight, moveWidth;
+
     Animator animator;
-    bool IsBlowing = false, IsMoving = false;
+    float movingHeight, movingWidth;
+    bool IsBlowing = false, IsMoving = false, moveHideFlag = false, LRmove = false;
+
+    //float TestTime = 0f;
     override protected void Start()
     {
+        movingWidth = moveWidth * -1;
+        movingHeight = moveHeight;
         animator = GetComponent<Animator>();
         base.Start();
     }
     override protected void Update()
     {
-        SlimeMove();
+        if(!IsBlowing) SlimeMove();
+
+        //TestTime += Time.deltaTime;
+        //Debug.Log("moving:" + IsMoving + "\nsecond:" + TestTime);
 
         animator.SetBool("IsMoving", IsMoving);
         animator.SetBool("IsBlowing", IsBlowing);
@@ -29,6 +41,55 @@ public class Slime : Enemy
 
     void SlimeMove()
     {
-        //à⁄ìÆèàóùÅB
+        //íÖínåvéZ
+
+
+        //îΩì]èàóù
+        if (IsMoving)
+        {
+            if (enemyRb.velocity.x == 0 && LRmove)
+            {
+                Vector2 scale = this.transform.localScale;
+                scale.x *= -1;
+                this.transform.localScale = scale;
+                movingWidth *= -1;
+                LRmove = false;
+            }
+        }
+
+        //àÍïbÇÃë“Çøèàóù
+        if (IsMoving && !moveHideFlag)
+        {
+            moveHideFlag = true;
+            StartCoroutine(SetMoveFalse());
+            //à⁄ìÆèàóùÅB
+            enemyRb.AddForce(new Vector2(movingWidth, movingHeight), ForceMode2D.Impulse);
+            LRmove = true;
+            StartCoroutine(Landing());
+        }
+        if (!IsMoving && !moveHideFlag)
+        {
+            moveHideFlag = true;
+            StartCoroutine(SetMoveTrue());
+        }
+
+        
+    }
+    IEnumerator Landing()
+    {
+        yield return new WaitForSeconds(0.5f);
+        LRmove = false;
+    }
+    IEnumerator SetMoveTrue()
+    {
+        yield return new WaitForSeconds(1f);
+        IsMoving = true;
+        moveHideFlag = false;
+    }
+    IEnumerator SetMoveFalse ()
+    {
+        yield return new WaitForSeconds(1f);
+        IsMoving = false;
+        moveHideFlag = false;
     }
 }
