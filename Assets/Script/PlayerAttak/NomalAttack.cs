b15cd56d7b630b7b06e0e32f816ac7ef8eb8637f
E21D2042 <st002122@m03.kyoto-kcg.ac.jp>
2023-05-16 15:52:08 +0900

    吹っ飛び修正

--- Assets/Script/PlayerAttak/NomalAttack.cs
+++ Assets/Script/PlayerAttak/NomalAttack.cs
@@ -12,7 +12,7 @@ public class NomalAttack : MonoBehaviour
     bool isAttack = false;
     bool isCoolTime = true;
 
-    Collider2D enemyCollider2D;
+    Collider2D enemyCollision;
 
     private void Start()
     {
@@ -30,40 +30,56 @@ public class NomalAttack : MonoBehaviour
 
     private void Update()
     {
+        //Žè“®UŒ‚FUŒ‚ƒ{ƒ^ƒ“‚ª‰Ÿ‚³‚ê‚¹‚½‚Æ‚«
         if (Input.GetKeyDown("joystick button 2") && isCoolTime && !playerController.isNomalAttackKay)
         {
             playerController.animator.SetTrigger("IsNomalAttack");
-            //playerController._Attack(, skill.damage);
+            if (enemyCollision != null)
+            {
+                playerController._Attack(enemyCollision, skill.damage);
+                
+            }
             StartCoroutine(_interval());
-            isCoolTime = false;
+        }
+
+        //Žè“®UŒ‚FUŒ‚ƒ{ƒ^ƒ“‚ª‰Ÿ‚³‚ê‚Ä‚¢‚éŠÔ
+        if (Input.GetKey("joystick button 2") && isCoolTime && !playerController.isNomalAttackKay)
+        {
+            playerController.animator.SetTrigger("IsNomalAttack");
+            if (enemyCollision != null)
+            {
+                playerController._Attack(enemyCollision, skill.damage);
+
+            }
+            StartCoroutine(_interval());
         }
     }
 
     //G‚ê‚Ä‚¢‚éŽž
     private void OnTriggerStay2D(Collider2D collision)
     {
+        enemyCollision = collision;
         if (collision.CompareTag("Enemy") && isAttack && isCoolTime && playerController.isNomalAttackKay)
         {
             //UŒ‚
             playerController.animator.SetTrigger("IsNomalAttack");
             playerController._Attack(collision, skill.damage);
             StartCoroutine(_interval());
-            isCoolTime = false;
         }
-        
     }
 
     //UŒ‚”ÍˆÍ‚©‚ç“G‚ª‚¢‚È‚­‚È‚Á‚½Žž
     private void OnTriggerExit2D(Collider2D collision)
     {
         isAttack = false;
+        enemyCollision = null;
     }
 
     //ƒN[ƒ‹ƒ^ƒCƒ€—pƒRƒ‹[ƒ`ƒ“
     IEnumerator _interval()
     {
         float time = skill.coolTime;
-
+        isCoolTime = false;
         while (time > 0)
         {
             time -= Time.deltaTime;
