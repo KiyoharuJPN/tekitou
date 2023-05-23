9b8da9f75064e67b7ffdde03992d3036f8c842a2
E21D2042 <st002122@m03.kyoto-kcg.ac.jp>
2023-05-23 21:38:55 +0900

    カメラ＋他修正

---
+++ Assets/Script/FadeImage.cs
@@ -0,0 +1,158 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeImage : MonoBehaviour
+{
+    [Header("最初からフェードインが完了しているかどうか")] public bool firstFadeInComp;
+
+    private Image img = null;
+    private int frameCount = 0;
+    private float timer = 0.0f;
+    private bool fadeIn = false;
+    private bool fadeOut = false;
+    private bool compFadeIn = false;
+    public bool compFadeOut = false;
+
+    /// <summary>
+    /// フェードインを開始する
+    /// </summary>
+    public void StartFadeIn()
+    {
+        if (fadeIn || fadeOut)
+        {
+            return;
+        }
+        fadeIn = true;
+        compFadeIn = false;
+        timer = 0.0f;
+        img.fillOrigin = (int)Image.OriginHorizontal.Right;
+        img.fillAmount = 1;
+        img.raycastTarget = true;
+    }
+
+    /// <summary>
+    /// フェードインが完了したかどうか
+    /// </summary>
+    /// <returns></returns>
+    public bool IsFadeInComplete()
+    {
+        return compFadeIn;
+    }
+
+    /// <summary>
+    /// フェードアウトを開始する
+    /// </summary>
+    public void StartFadeOut()
+    {
+        if (fadeIn || fadeOut)
+        {
+            return;
+        }
+        fadeOut = true;
+        compFadeOut = false;
+        timer = 0.0f;
+        img.fillOrigin = (int)Image.OriginHorizontal.Left;
+        img.fillAmount = 0f;
+        img.raycastTarget = true;
+    }
+
+    /// <summary>
+    /// フェードアウトが完了したかどうか
+    /// </summary>
+    /// <returns></returns>
+    public bool IsFadeOutComplete()
+    {
+        return compFadeOut;
+    }
+
+    void Start()
+    {
+        img = GetComponent<Image>();
+        if (firstFadeInComp)
+        {
+            FadeInComplete();
+        }
+        else
+        {
+            StartFadeIn();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.H))
+        {
+            StartFadeOut();
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            StartFadeIn();
+        }
+
+        if (frameCount > 2)
+        {
+            if (fadeIn)
+            {
+                FadeInUpdate();
+            }
+            else if (fadeOut)
+            {
+                FadeOutUpdate();
+            }
+        }
+        ++frameCount;
+    }
+
+    //フェードイン中
+    private void FadeInUpdate()
+    {
+        //フェード中
+        if (timer < 1f)
+        {
+            img.fillAmount -= 1f * Time.deltaTime;
+        }
+        //フェードが完了したとき
+        else
+        {
+            FadeInComplete();
+        }
+        timer += Time.deltaTime;
+    }
+    //フェードアウト中
+    private void FadeOutUpdate()
+    {
+        if (timer < 1f)
+        {
+            img.fillAmount += 1f * Time.deltaTime;
+        }
+        else
+        {
+            FadeOutComplete();
+        }
+        timer += Time.deltaTime;
+    }
+
+    //フェードイン完了
+    private void FadeInComplete()
+    {
+        img.fillAmount = 0f;
+        img.raycastTarget = false;
+        timer = 0f;
+        fadeIn = false;
+        compFadeIn = true;
+    }
+
+    //フェードアウト完了
+    private void FadeOutComplete()
+    {
+        img.fillAmount = 1f;
+        img.raycastTarget = false;
+        timer = 0f;
+        fadeOut = false;
+        compFadeOut = true;
+    }
+}
---
+++ Assets/Script/Player/PlayerAction/Player_IsGround.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_IsGround : MonoBehaviour
+{
+    //親のオブジェクト
+    [SerializeField]
+    GameObject HERO;
+
+    PlayerController player;
+    Player_Jump jumpData;
+
+    void Start()
+    {
+        player = HERO.GetComponent<PlayerController>();
+        jumpData = HERO.GetComponent<Player_Jump>();
+    }
+
+    //着地の判定
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Stage"))
+        {
+            if (player.isUpAttack)
+            {
+                return;
+            }
+            if (player.isFalling == true)
+            {
+                player.isLanding = true;
+            }
+            player.isSquatting = false;
+            player.isJumping = false;
+
+            player.rb.velocity = Vector2.zero;
+            jumpData.jumpTime = 0;
+            jumpData.canSecondJump = false;
+
+            //突き刺し攻撃終わり
+            if (player.isDropAttack)
+            {
+                jumpData.shake.Shake(jumpData._shakeInfo.Duration, jumpData._shakeInfo.Strength);
+                Invoke(nameof(DropAttackOff), 0.5f);
+            };
+
+            Invoke(nameof(Landingoff), 0.1f);
+        }
+    }
+
+    void Landingoff()
+    {
+        player.isLanding = false;
+    }
+
+    void DropAttackOff()
+    {
+        player.isDropAttack = false;
+        player.animator.SetBool("IsDropAttack", player.isDropAttack);
+    }
+}
