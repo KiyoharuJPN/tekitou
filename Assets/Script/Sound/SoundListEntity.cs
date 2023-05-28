fe6ee9c67e282e4dd9e8f03cdec86a4cb9dccd72
LAPTOP-KIYOHARU\kiyoharu <1905088203@qq.com>
2023-05-28 19:29:16 +0900

    いらないデータの修正削除

--- Assets/Script/Sound/SoundListEntity.cs
+++
@@ -1,8 +0,0 @@
-/*using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "ScriptableObject/SoundData")]
-public class SoundListEntity : ScriptableObject
-{
-    public List<SoundData> SoundDataList = new List<SoundData>();
-}*/
