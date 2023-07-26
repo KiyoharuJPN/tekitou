22df1cc29ad89fc60cdf1c62a94f2947fe925619
LAPTOP-KIYOHARU\kiyoharu <1905088203@qq.com>
2023-07-26 18:22:56 +0900

    モンスターコライダーと無敵時間の追加や修正

--- Assets/Script/Enemy/EnemyBase/EnemyColliderCheck.cs
+++ Assets/Script/Enemy/EnemyBase/EnemyColliderCheck.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class EnemyColliderCheck : MonoBehaviour
 {
+    //トリガーに入る時にコライダーの動作をする
     private void OnTriggerEnter2D(Collider2D collision)
     {
         GetComponentInParent<Enemy>().OnColEnter(collision);
     }
 
+    //トリガーにい続けるときにコライダーの動作をする
     private void OnTriggerStay2D(Collider2D collision)
     {
         GetComponentInParent<Enemy>().OnColStay(collision);
