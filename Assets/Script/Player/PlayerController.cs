01b697e17838f3d8bb767200f8c4886cfd0ee8db
LAPTOP-KIYOHARU\kiyoharu <1905088203@qq.com>
2023-08-23 14:34:55 +0900

    キングスライム修正

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -561,7 +561,7 @@ public class PlayerController : MonoBehaviour
             {
                 sprite.color = new Color(1, .3f, .3f);
             }
-            yield return new WaitForSeconds(0.05f);
+            yield return new WaitForSeconds(0.01f);
         }
         inInvincibleTimeKnockBack = false;
     }
@@ -572,7 +572,7 @@ public class PlayerController : MonoBehaviour
         while (n > 0)
         {
             n--;
-            yield return new WaitForSeconds(0.05f);
+            yield return new WaitForSeconds(0.01f);
         }
         inInvincibleTimeLife = false;
     }
